7246fcad20b12a2d4eaf02b1f2689d8f613d3181
zzeromin <zzeromin@gmail.com>
2014-07-31 13:41:49 +0900

    Update RGB_LED.ino
    실습용 예제

--- WISET_Code/RGB_LED/RGB_LED.ino
+++ WISET_Code/RGB_LED/RGB_LED.ino
@@ -15,6 +15,20 @@ void loop()
 {
   setColor(255, 0, 0); // red
   delay(1000);
+}
+ 
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue); 
+}
+
+/*
+void loop()
+{
+  setColor(255, 0, 0); // red
+  delay(1000);
   setColor(0, 255, 0); // green
   delay(1000);
   setColor(0, 0, 255); // blue
@@ -26,10 +40,4 @@ void loop()
   setColor(0, 255, 255); // aqua
   delay(1000);
 }
- 
-void setColor(int red, int green, int blue)
-{
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue); 
-}
+*/
