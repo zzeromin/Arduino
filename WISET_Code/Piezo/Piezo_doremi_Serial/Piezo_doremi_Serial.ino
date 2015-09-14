bc901ebe56f37dc1c70fb99e18d2ee68a4f961f9
zzeromin <zzeromin@gmail.com>
2015-09-14 16:12:51 +0900

    Update Piezo_doremi_Serial.ino

--- WISET_Code/Piezo/Piezo_doremi_Serial/Piezo_doremi_Serial.ino
+++ WISET_Code/Piezo/Piezo_doremi_Serial/Piezo_doremi_Serial.ino
@@ -17,16 +17,16 @@ void loop()
   if( Serial.available() > 0 )
   {
     int note = Serial.read();
-    if(note == '1') tone(2, NOTE_C7);
-    else if(note == '2') tone(2, NOTE_D7);
-    else if(note == '3') tone(2, NOTE_E7);
-    else if(note == '4') tone(2, NOTE_F7);
-    else if(note == '5') tone(2, NOTE_G7);
-    else if(note == '6') tone(2, NOTE_A7);
-    else if(note == '7') tone(2, NOTE_B7);
-    else if(note == '8') tone(2, NOTE_C8);
-    else if(note == '0') noTone(2);   
+    if(note == '1') tone(8, NOTE_C7);
+    else if(note == '2') tone(8, NOTE_D7);
+    else if(note == '3') tone(8, NOTE_E7);
+    else if(note == '4') tone(8, NOTE_F7);
+    else if(note == '5') tone(8, NOTE_G7);
+    else if(note == '6') tone(8, NOTE_A7);
+    else if(note == '7') tone(8, NOTE_B7);
+    else if(note == '8') tone(8, NOTE_C8);
+    else if(note == '0') noTone(8);   
     delay(300); 
-    noTone(2);
+    noTone(8);
   }
 }
