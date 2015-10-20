93905f5830e4c30b558f1a9b9f09c2b182e2e814
zzeromin <zzeromin@gmail.com>
2015-10-20 11:54:35 +0900

    Create CapacitiveLED.ino

---
+++ WISET_Code/CapacitiveSensor/CapacitiveLED.ino
@@ -0,0 +1,30 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor sensor = CapacitiveSensor(2, 3);
+
+int th = 120;
+int LED = 13;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  // turn off autocalibrate on channel 1 - just as an example
+}
+
+void loop()
+{
+  long sensorValue = sensor.capacitiveSensor(30);
+  Serial.println(sensorValue);
+
+  if (sensorValue > th)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+  delay(10);
+}
