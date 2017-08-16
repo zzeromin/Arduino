3172921186528fbb148eb28b229e9510523fc7d6
Youngmin Seo <zzeromin@gmail.com>
2017-08-16 14:28:07 +0900

    Update NeoPixel_Piezo_Festival.ino

--- WISET_Code/NeoPixel_Piezo_Festival/NeoPixel_Piezo_Festival.ino
+++ WISET_Code/NeoPixel_Piezo_Festival/NeoPixel_Piezo_Festival.ino
@@ -2,9 +2,7 @@
   Title        : NeoPixel_Piezo_Touch.ino
   Author       : zzeromin, member of Raspberrypi Village and Tentacle Team
   Creation Date: Aug 5, 2017
-  Blog         : http://rasplay.org, http://forums.rasplay.org/, https://zzeromin.tumblr.com/
-  Github       : https://github.com/rasplay, https://github.com/zzeromin
-  Cafe         : http://cafe.naver.com/raspigamer
+  Homepage     : http://wise.duksung.ac.kr
   reference    : https://kocoafab.cc/tutorial/view/461
 */
 
