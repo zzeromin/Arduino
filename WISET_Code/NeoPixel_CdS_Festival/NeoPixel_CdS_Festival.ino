e49e8212e980b93cfc829006799e47108e612e38
Youngmin Seo <zzeromin@gmail.com>
2017-08-16 14:27:42 +0900

    Update NeoPixel_CdS_Festival.ino

--- WISET_Code/NeoPixel_CdS_Festival/NeoPixel_CdS_Festival.ino
+++ WISET_Code/NeoPixel_CdS_Festival/NeoPixel_CdS_Festival.ino
@@ -2,9 +2,7 @@
 Title        : NeoPixel_CdS_Festival.ino
 Author       : zzeromin, member of Raspberrypi Village and Tentacle Team
 Creation Date: Aug 5, 2017
-Blog         : http://rasplay.org, http://forums.rasplay.org/, https://zzeromin.tumblr.com/
-Github       : https://github.com/rasplay, https://github.com/zzeromin
-Cafe         : http://cafe.naver.com/raspigamer
+Homepage     : http://wise.duksung.ac.kr
 */
 
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
